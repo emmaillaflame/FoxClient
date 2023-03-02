dcba0c26d3f64b4cf6a44691b6a37e260764a809
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-03-02 17:50:43 -0500

    ⬆️ Transferred from Codepen

---
+++ script.js
@@ -0,0 +1,7 @@
+function five(){
+  window.location.replace("https://itsfoxdev.github.io/FoxClient/5/");
+}
+
+function eight(){
+  window.location.replace("https://itsfoxdev.github.io/FoxClient/8/");
+}
