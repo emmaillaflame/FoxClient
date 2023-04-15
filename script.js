72deba2fc8c9b8b547a3be64095b3f4ea0dec293
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-04-15 10:37:42 -0400

    🎉 New animation

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function five(){
 function eight(){
   window.location.replace("https://itsfoxdev.github.io/FoxClient/8/");
 }
+
+function loadAnim(){
+  var l = document.getElementById('animi');
+  var t = document.getElementById('animt');
+  var c = document.getElementById('animc');
+  setTimeout(function(){l.style.scale=1}, 10);
+  setTimeout(function(){t.style.width="302px"}, 510);
+  setTimeout(function(){c.style.height="0px"}, 2000);
+}
+loadAnim();
